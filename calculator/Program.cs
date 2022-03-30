da0f66fd42e73196eacd2f980b300102e8c2f2e5
ThreeAi <alexandrovaa2003@gmail.com>
2022-03-30 17:07:20 +0300

    3

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -6,16 +6,24 @@ namespace calculator
     {
         static void Main(string[] args)
         {
-            string str;                                              
-            str = "7*(4/8) ";
+            string str;
+            Console.WriteLine("Введите арифментической выражение (без пробелов)");
+            str = Console.ReadLine();
             Console.WriteLine(Calc(str));
         }
         public static double Calc(string str)
         {
+            int[,] mas = new int[,] { { 6, 1, 1, 1, 1, 1, 5 },
+                                      { 5, 1, 1, 1, 1, 1, 3 },
+                                      { 4, 1, 2, 2, 1, 1, 4 },
+                                      { 4, 1, 2, 2, 1, 1, 4 },
+                                      { 4, 1, 4, 4, 2, 2, 4 },
+                                      { 4, 1, 4, 4, 2, 2, 4 } };
             int ind1 = 0, ind2 = 0;
             MyStack stE = new MyStack();
             MyStack stT = new MyStack();
-            string num = "";            
+            string num = "";
+            str += " ";
             for (int i = 0; i < str.Length; i++)
             {
                 if ((str[i] != ' ') && (str[i] != '*') && (str[i] != '/') && (str[i] != '+') && (str[i] != '-') && (str[i] != '(') && (str[i] != ')'))
@@ -25,53 +33,41 @@ namespace calculator
                     {
                         stE.Push(Convert.ToString(num));
                         num = "";
-                    }                   
+                    }
                 }
                 else
                 {
-                    ind2 = Define(str[i]);  
+                    ind2 = Define(str[i]);
                     ind1 = Define(Convert.ToChar(stT.Peek()));
-                    Action(ind1, ind2, str[i], stT, stE);                                           
-                }              
+                    Action(ind1, ind2, str[i], stT, stE, mas);
+                }
             }
             return Convert.ToDouble(stE.Pop());
         }
-        public static void Action(int ind1, int ind2, char c, MyStack stT, MyStack stE)
+        public static void Action(int ind1, int ind2, char c, MyStack stT, MyStack stE, int[,] mas)
         {
-            int[,] mas = new int[,] { { 6, 1, 1, 1, 1, 1, 5 },
-                                      { 5, 1, 1, 1, 1, 1, 3 },
-                                      { 4, 1, 2, 2, 1, 1, 4 },
-                                      { 4, 1, 2, 2, 1, 1, 4 },
-                                      { 4, 1, 4, 4, 2, 2, 4 },
-                                      { 4, 1, 4, 4, 2, 2, 4 } };
             switch (mas[ind1, ind2])
             {
                 case 1:
-                    Console.WriteLine(1);
-                    f1(c, stT);                    
+                    f1(c, stT);
                     break;
                 case 2:
-                    Console.WriteLine(2);
                     f2(c, stT, stE);
                     break;
                 case 3:
-                    Console.WriteLine(3);
                     f3(stT);
                     break;
                 case 4:
-                    Console.WriteLine(4);
-                    f4(c, stT, stE);
+                    f4(stT, stE);
+                    Action(Define(Convert.ToChar(stT.Peek())), Define(c), c, stT, stE, mas);
                     break;
                 case 5:
-                    Console.WriteLine(5);
-                    Console.WriteLine("Error");
+                    Console.WriteLine("Ошибка в выражении");
+                    Environment.Exit(0);
                     break;
-                case 6:
-                    Console.WriteLine(6);
-                    break;                    
             }
         }
-        public static int Define (char oper)
+        public static int Define(char oper)
         {
             int res = -1;
             switch (oper)
@@ -114,7 +110,13 @@ namespace calculator
                     stE.Push(Convert.ToString(a + b));
                     break;
                 case '/':
-                    stE.Push(Convert.ToString(a / b));
+                    if (b != 0)
+                        stE.Push(Convert.ToString(a / b));
+                    else
+                    {
+                        Console.WriteLine("Делить на ноль нельзя");
+                        Environment.Exit(0);
+                    }
                     break;
                 case '-':
                     stE.Push(Convert.ToString(a - b));
@@ -134,10 +136,9 @@ namespace calculator
         {
             stT.Del();
         }
-        public static void f4(char oper, MyStack stT, MyStack stE)
+        public static void f4(MyStack stT, MyStack stE)
         {
             Operation(stT, stE);
-            Action(Define(Convert.ToChar(stT.Peek())), Define(oper), oper, stT, stE);
         }
     }
 
